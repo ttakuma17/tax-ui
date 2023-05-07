6e266642c5ce65adf075544153022755f606ffa2
ttakuma17 <ttanimiya@gmail.com>
2023-05-07 13:28:11 +0900

    React Hook Formの検証

--- src/InputForm.tsx
+++ src/InputForm.tsx
@@ -1,6 +1,22 @@
+import { useForm } from "react-hook-form";
+
 import { Button, Card, CardBody, CardHeader, CardProps, Center, Checkbox, FormControl, FormHelperText, FormLabel, Heading, HStack, Input, InputGroup, InputRightAddon, Radio, RadioGroup, Spacer, Stack, VStack } from "@chakra-ui/react";
 
-export const InputForm = ({ ...props}: CardProps) => {
+
+type FormInputs = {
+    yearsOfService: string
+    isDisability: boolean
+    isExecutive: boolean
+    severancePay: string
+}
+
+export const InputForm = ({ ...props }: CardProps) => {
+    const { register, handleSubmit } = useForm<FormInputs>();
+
+    const onInputFormSubmit = (formInputs: FormInputs) => { 
+        console.log(formInputs);
+    }
+
     return (
         <Card w={400} {...props}>
             <CardHeader>
@@ -9,13 +25,13 @@ export const InputForm = ({ ...props}: CardProps) => {
                 </Center>
             </CardHeader>
             <CardBody>
-                <form>
+                <form onSubmit={handleSubmit(onInputFormSubmit)}>
                     <VStack spacing={5}>
                         <FormControl>
                             <FormLabel fontWeight="bold">勤続年数</FormLabel>
                             <HStack>
                                 <InputGroup w="120px">
-                                    <Input type="number" defaultValue="10" />
+                                    <Input type="number" defaultValue="10" {...register("yearsOfService")} />
                                     <InputRightAddon>年</InputRightAddon>
                                 </InputGroup>
                         <FormHelperText>1年未満の端数は切り上げ</FormHelperText> </HStack>
@@ -23,20 +39,20 @@ export const InputForm = ({ ...props}: CardProps) => {
                         </FormControl>
                         <FormControl>
                             <FormLabel fontWeight="bold">退職基因</FormLabel>
-                            <Checkbox>障害者となったことに直接基因して退職した</Checkbox>
+                            <Checkbox {...register("isDisability")} >障害者となったことに直接基因して退職した</Checkbox>
                         </FormControl>
                         <FormControl>
                             <FormLabel fontWeight="bold">役員等以外か役員等か</FormLabel>
                             <RadioGroup defaultValue="0">
                                 <Stack direction="row">
-                                    <Radio value="0">役員等以外</Radio>
-                                    <Radio value="1">役員等</Radio>
+                                    <Radio value="0" {...register("isExecutive")}>役員等以外</Radio>
+                                    <Radio value="1" {...register("isExecutive")}>役員等</Radio>
                                 </Stack>
                             </RadioGroup>
                         </FormControl>
                         <FormControl>
                             <FormLabel fontWeight="bold">退職金</FormLabel> <InputGroup w="200px">
-                            <Input type="number" defaultValue="5000000" />
+                            <Input type="number" defaultValue="5000000" {...register("severancePay")} />
                             <InputRightAddon>円</InputRightAddon> </InputGroup>
                         </FormControl>
                         <Button colorScheme="blue" alignSelf="flex-end" type="submit">
